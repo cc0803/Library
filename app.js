6b445a877eb757d7d63ca7656b93d4fa881cd1d8
cc0803 <simongeldbach@gmail.com>
2023-03-27 17:37:45 +0200

    Copy files from computer to repo

---
+++ app.js
@@ -0,0 +1,28 @@
+let library = [];
+
+function BookConstructor(title, author, pages, read){
+    this.title = title,
+    this.author = author,
+    this.pages =  pages,
+    this.read = read
+}
+
+BookConstructor.prototype.info = function() {
+    return `${this.title} from ${this.author} has ${this.pages}. ${this.read}`;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    library.push(new BookConstructor(title, author, pages, read));
+}
+
+function displayLibrary() {
+    for (let book of library) {
+        console.log(book.info())
+    }
+}
+
+addBookToLibrary('Harry Potter', 'J.K. Rowling', 576, 'not read');
+addBookToLibrary('Tributes of Panem', 'Susanne Collins', 532, 'not read');
+addBookToLibrary('Atomic Habbits', 'James Clear', 352, 'read');
+
+displayLibrary();
