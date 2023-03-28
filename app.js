a82ea761bf89e088e9b8da15ac4a34e02178a0cb
cc0803 <simongeldbach@gmail.com>
2023-03-28 17:20:15 +0200

    Add and style form and overlay

--- app.js
+++ app.js
@@ -1,62 +1,66 @@
-
 let library = [];
 
 // Select all relevant document elements
-const showButton = document.querySelector('.show');
-const bookDisplay = document.querySelector('.display');
+const showButton = document.querySelector(".show");
+const bookDisplay = document.querySelector(".display");
+const addButton = document.querySelector(".add");
 
-function BookConstructor(title, author, pages, read){
-    this.title = title,
-    this.author = author,
-    this.pages =  pages,
-    this.read = read
+function BookConstructor(title, author, pages, read) {
+    (this.title = title),
+        (this.author = author),
+        (this.pages = pages),
+        (this.read = read);
 }
 
-BookConstructor.prototype.info = function() {
+BookConstructor.prototype.info = function () {
     return `${this.title} from ${this.author} has ${this.pages}. ${this.read}`;
-}
+};
 
 function addBookToLibrary(title, author, pages, read) {
     library.push(new BookConstructor(title, author, pages, read));
 }
 
-addBookToLibrary('Harry Potter', 'J.K. Rowling', 576, 'not read');
-addBookToLibrary('Tributes of Panem', 'Susanne Collins', 532, 'not read');
-addBookToLibrary('Atomic Habbits', 'James Clear', 352, 'read');
+addBookToLibrary("Harry Potter", "J.K. Rowling", 576, "not read");
+addBookToLibrary("Tributes of Panem", "Susanne Collins", 532, "not read");
+addBookToLibrary("Atomic Habbits", "James Clear", 352, "read");
 
 function displayLibrary() {
-    library.forEach(book => {
+    library.forEach((book) => {
         // Create new div.book as a container for the book
-        let newElement = document.createElement('div');
-        newElement.classList.add('book', 'flex-center');
+        let newElement = document.createElement("div");
+        newElement.classList.add("book", "flex-center");
         bookDisplay.appendChild(newElement);
 
         // Add the heading and description and match them to books in the library
-        let heading = document.createElement('h3');
+        let heading = document.createElement("h3");
         heading.textContent = book.title;
         newElement.appendChild(heading);
 
         // Add author, pages and rather I have read it
-        let author = document.createElement('p')
-        let pages = document.createElement('p')
-        let read = document.createElement('p')
+        let author = document.createElement("p");
+        let pages = document.createElement("p");
+        let read = document.createElement("p");
 
-        author.classList.add('author');
-        pages.classList.add('pages');
-        read.classList.add('read');
-        
+        author.classList.add("author");
+        pages.classList.add("pages");
+        read.classList.add("read");
+
         author.textContent = book.author;
         pages.textContent = book.pages;
         read.textContent = book.read;
 
-        newElement.appendChild(author)
-        newElement.appendChild(pages)
-        newElement.appendChild(read)
+        newElement.appendChild(author);
+        newElement.appendChild(pages);
+        newElement.appendChild(read);
 
         bookDisplay.style.backgroundColor = "#555";
     });
 }
 
-showButton.addEventListener('click', () => {
-    displayLibrary();
-}, {once: true});
+showButton.addEventListener(
+    "click",
+    () => {
+        displayLibrary();
+    },
+    { once: true }
+);
