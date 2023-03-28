114c285d20b9dfb4369f92e790660f9a41db6a5a
cc0803 <simongeldbach@gmail.com>
2023-03-28 12:44:00 +0200

    Install prettier and format files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Library
+
 This project displays different books and some basic information about them.
 
 The use of this project is to practice objects, Constructors and prototypes.
--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 let library = [];
 
-function BookConstructor(title, author, pages, read){
-    this.title = title,
-    this.author = author,
-    this.pages =  pages,
-    this.read = read
+// Select all relevant document elements
+const showButton = document.querySelector(".show");
+const bookDisplay = document.querySelector(".display");
+
+function BookConstructor(title, author, pages, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read);
 }
 
-BookConstructor.prototype.info = function() {
-    return `${this.title} from ${this.author} has ${this.pages}. ${this.read}`;
-}
+BookConstructor.prototype.info = function () {
+  return `${this.title} from ${this.author} has ${this.pages}. ${this.read}`;
+};
 
 function addBookToLibrary(title, author, pages, read) {
-    library.push(new BookConstructor(title, author, pages, read));
+  library.push(new BookConstructor(title, author, pages, read));
 }
 
 function displayLibrary() {
-    for (let book of library) {
-        console.log(book.info())
-    }
+  for (let book of library) {
+    console.log(book.info());
+  }
 }
 
-addBookToLibrary('Harry Potter', 'J.K. Rowling', 576, 'not read');
-addBookToLibrary('Tributes of Panem', 'Susanne Collins', 532, 'not read');
-addBookToLibrary('Atomic Habbits', 'James Clear', 352, 'read');
+addBookToLibrary("Harry Potter", "J.K. Rowling", 576, "not read");
+addBookToLibrary("Tributes of Panem", "Susanne Collins", 532, "not read");
+addBookToLibrary("Atomic Habbits", "James Clear", 352, "read");
 
 displayLibrary();
+
+showButton.addEventListener("click", () => {
+  let newElement = document.createElement("div");
+  newElement.classList.add("book");
+  bookDisplay.appendChild(newElement);
+});
